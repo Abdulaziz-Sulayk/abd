366cfb1d7df1e6f6edd566b83c58709c559bb8d9
Abdulaziz-Sulayk <124029370+Abdulaziz-Sulayk@users.noreply.github.com>
2023-02-04 18:10:34 +0300

    add files

---
+++ javasc.js
@@ -0,0 +1,197 @@
+var maindiv= document.createElement('div');
+document.body.appendChild(maindiv);
+maindiv.style.display= "flex";
+maindiv.style.flexWrap = "wrap";
+maindiv.style.justifyContent = "space-between";
+maindiv.style.fontFamily="sans-serif";
+maindiv.style.backgroundColor="rgb(34, 30, 30)";
+maindiv.style.borderRadius= " 10px";
+maindiv.style.padding="10px";
+
+
+var mydiv1= document.createElement('div');
+ maindiv.appendChild(mydiv1);
+
+var myimg= document.createElement('img');
+ myimg.setAttribute("src" , "img/1.jpeg");
+ mydiv1.appendChild(myimg);
+
+ var myh4= document.createElement('h4');
+ mytext= document.createTextNode('مشاوي') 
+ myh4.appendChild(mytext);
+mydiv1.appendChild(myh4);
+
+myh4.style.backgroundColor= "#e6ae14";
+myh4.style.margin= "0";
+myh4.style.textAlign= "center";
+myh4.style.borderRadius= "0px 0px 10px 10px";
+myimg.style.borderRadius= "10px 10px 0px 0px";
+myh4.style.fontSize="20px";
+myh4.style.paddingTop="5px";
+
+
+// jkzhvzhvsodh;o oishuiso\h ufhrueighwuoghwiughwuighwui
+
+var mydiv2= document.createElement('div');
+maindiv.appendChild(mydiv2);
+
+var myimg= document.createElement('img');
+myimg.setAttribute("src" , "img/2.jpeg");
+mydiv2.appendChild(myimg);
+
+myh4= document.createElement('h4');
+mytext= document.createTextNode('شاورما')
+
+myh4.appendChild(mytext);
+mydiv2.appendChild(myh4);
+myh4.style.backgroundColor= "#e6ae14";
+myh4.style.margin= "0";
+myh4.style.textAlign= "center";
+myh4.style.borderRadius= "0px 0px 10px 10px";
+myimg.style.borderRadius= "10px 10px 0px 0px";
+myh4.style.fontSize="20px";
+myh4.style.paddingTop="5px";
+
+// jkzhvzhvsodh;o oishuiso\h ufhrueighwuoghwiughwuighwui
+
+
+var mydiv3= document.createElement('div');
+
+maindiv.appendChild(mydiv3);
+
+var myimg= document.createElement('img');
+ myimg.setAttribute("src" , "img/3.jpeg");
+ mydiv3.appendChild(myimg);
+
+myh4= document.createElement('h4');
+ mytext= document.createTextNode('كبب')
+ 
+ myh4.appendChild(mytext);
+mydiv3.appendChild(myh4);
+myh4.style.backgroundColor= "#e6ae14";
+myh4.style.margin= "0";
+myh4.style.textAlign= "center";
+myh4.style.borderRadius= "0px 0px 10px 10px";
+myimg.style.borderRadius= "10px 10px 0px 0px";
+myh4.style.fontSize="20px";
+myh4.style.paddingTop="5px";
+
+// jkzhvzhvsodh;o oishuiso\h ufhrueighwuoghwiughwuighwui
+
+
+var mydiv4= document.createElement('div');
+
+maindiv.appendChild(mydiv4);
+
+var myimg= document.createElement('img');
+myimg.setAttribute("src" , "img/4.jpeg");
+mydiv4.appendChild(myimg);
+
+myh4= document.createElement('h4');
+mytext= document.createTextNode('رز')
+
+myh4.appendChild(mytext);
+mydiv4.appendChild(myh4);
+myh4.style.backgroundColor= "#e6ae14";
+myh4.style.margin= "0";
+myh4.style.textAlign= "center";
+myh4.style.borderRadius= "0px 0px 10px 10px";
+myimg.style.borderRadius= "10px 10px 0px 0px";
+myh4.style.fontSize="20px";
+myh4.style.paddingTop="5px";
+
+// jkzhvzhvsodh;o oishuiso\h ufhrueighwuoghwiughwuighwui
+
+var mydiv5= document.createElement('div');
+
+maindiv.appendChild(mydiv5);
+
+var myimg= document.createElement('img');
+myimg.setAttribute("src" , "img/5.jpeg");
+mydiv5.appendChild(myimg);
+
+myh4= document.createElement('h4');
+mytext= document.createTextNode('كبسة')
+
+myh4.appendChild(mytext);
+mydiv5.appendChild(myh4);
+myh4.style.backgroundColor= "#e6ae14";
+myh4.style.margin= "0";
+myh4.style.textAlign= "center";
+myh4.style.borderRadius= "0px 0px 10px 10px";
+myimg.style.borderRadius= "10px 10px 0px 0px";
+myh4.style.fontSize="20px";
+myh4.style.paddingTop="5px";
+
+
+// jkzhvzhvsodh;o oishuiso\h ufhrueighwuoghwiughwuighwui
+
+var mydiv6= document.createElement('div');
+
+maindiv.appendChild(mydiv6);
+
+var myimg= document.createElement('img');
+myimg.setAttribute("src" , "img/6.jpeg");
+mydiv6.appendChild(myimg);
+
+myh4= document.createElement('h4');
+mytext= document.createTextNode('رزاية')
+
+myh4.appendChild(mytext);
+mydiv6.appendChild(myh4);
+myh4.style.backgroundColor= "#e6ae14";
+myh4.style.margin= "0";
+myh4.style.textAlign= "center";
+myh4.style.borderRadius= "0px 0px 10px 10px";
+myimg.style.borderRadius= "10px 10px 0px 0px";
+myh4.style.fontSize="20px";
+myh4.style.paddingTop="5px";
+
+
+// jkzhvzhvsodh;o oishuiso\h ufhrueighwuoghwiughwuighwui
+
+var mydiv7= document.createElement('div');
+
+maindiv.appendChild(mydiv7);
+
+var myimg= document.createElement('img');
+myimg.setAttribute("src" , "img/7.jpeg");
+mydiv7.appendChild(myimg);
+
+myh4= document.createElement('h4');
+mytext= document.createTextNode('كباب')
+
+myh4.appendChild(mytext);
+mydiv7.appendChild(myh4);
+myh4.style.backgroundColor= "#e6ae14";
+myh4.style.margin= "0";
+myh4.style.textAlign= "center";
+myh4.style.borderRadius= "0px 0px 10px 10px";
+myimg.style.borderRadius= "10px 10px 0px 0px";
+myh4.style.fontSize="20px";
+myh4.style.paddingTop="5px";
+
+// jkzhvzhvsodh;o oishuiso\h ufhrueighwuoghwiughwuighwui
+
+var mydiv8= document.createElement('div');
+
+maindiv.appendChild(mydiv8);
+
+var myimg= document.createElement('img');
+myimg.setAttribute("src" , "img/8.jpeg");
+mydiv8.appendChild(myimg);
+
+myh4= document.createElement('h4');
+mytext= document.createTextNode('فروج')
+
+myh4.appendChild(mytext);
+mydiv8.appendChild(myh4);
+myh4.style.backgroundColor= "#e6ae14";
+myh4.style.margin= "0";
+myh4.style.textAlign= "center";
+myh4.style.borderRadius= "0px 0px 10px 10px";
+myimg.style.borderRadius= "10px 10px 0px 0px";
+myh4.style.fontSize="20px";
+myh4.style.paddingTop="5px";
+
+
